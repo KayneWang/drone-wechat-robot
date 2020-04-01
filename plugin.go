0aae73695a0358c260be9336a71a8fb93f0e903c
KayneWang <w.zengkai@foxmail.com>
2020-04-01 21:58:47 +0800

    支持image

--- plugin.go
+++ plugin.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"bytes"
+	"crypto/md5"
+	"encoding/base64"
 	"encoding/json"
 	"errors"
 	"fmt"
@@ -37,6 +39,8 @@ type (
 		Content             string   `json:"content"`
 		MentionedList       []string `json:"mentioned_list"`
 		MentionedMobileList []string `json:"mentioned_mobile_list"`
+		Base64              string   `json:"base64"`
+		Md5                 string   `json:"md5"`
 	}
 
 	Job struct {
@@ -51,14 +55,20 @@ type (
 	}
 )
 
-type TextContent struct {
-	Content             string   `json:"content"`
-	MentionedList       []string `json:"mentioned_list"`
-	MentionedMobileList []string `json:"mentioned_mobile_list"`
-}
-type MarkdownContent struct {
-	Content string `json:"content"`
-}
+type (
+	TextContent struct {
+		Content             string   `json:"content"`
+		MentionedList       []string `json:"mentioned_list"`
+		MentionedMobileList []string `json:"mentioned_mobile_list"`
+	}
+	MarkdownContent struct {
+		Content string `json:"content"`
+	}
+	ImageContent struct {
+		Base64 base64 `json:"base64"`
+		Md5    md5    `json:"md5"`
+	}
+)
 
 func (p Plugin) Exec() error {
 	url := "https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=" + p.Config.Key
@@ -87,11 +97,28 @@ func (p Plugin) Exec() error {
 			Markdown MarkdownContent `json:"markdown"`
 		}{p.Config.MsgType, markdown}
 
+	case "image":
+		b64, err := base64.StdEncoding.DecodeString(p.Config.Base64)
+		m5 := md5.Sum([]byte(p.Config.Md5))
+		if err != nil {
+			fmt.Printf("Error: failed to encoding base64")
+			return err
+		}
+		image := ImageContent{
+			Base64: b64,
+			Md5:    m5,
+		}
+		data = struct {
+			MsgType string       `json:"msgtype"`
+			Image   ImageContent `json:"image"`
+		}{p.Config.MsgType, image}
+
 	default:
 		return errors.New("Error: wrong msgtype, you should use either text, markdown, image, news")
 	}
 
 	b, _ := json.Marshal(data)
+	fmt.Printf("Request data: %s\n", string(b))
 
 	request, err := http.NewRequest("POST", url, bytes.NewBuffer(b))
 	if err != nil {
